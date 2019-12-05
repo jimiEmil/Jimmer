4343cd7f56f16d0cbdfba962756ee19aaea25c0a
Tobias Immersen <timmersen@gmail.com>
2019-12-05 14:09:42 +0100

    Jimmer the beninging

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+}
+
+let mapSizeX = 1000;
+let mapSizeY = 600;
+
+let startPos = (0,0)
+
+let starSize = Math.random(2,9)
+let starPosX
+let starPosX
+function draw() {
+  
+  background(220);
+  translate(width/2,height/2)
+  rectMode(CENTER)
+
+  // Dette er vores map
+  fill(0,0,0)
+   rect(0,0,mapSizeX,mapSizeY); 
+
+  fill(100)
+   rect(0, 0, 100, 100);
+
+cicle 
+
+
+}
