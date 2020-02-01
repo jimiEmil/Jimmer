c06f86d1ff39abdf7fdc94dcfc28721685921090
JimiZakariassen <zakariassenjimi@gmail.com>
2020-02-01 13:02:31 +0100

    alt virker indtil 23.28. ingen fejl.

--- jimAstroids/sketch.js
+++ jimAstroids/sketch.js
@@ -23,9 +23,11 @@ function draw() {
     lasers[i].update(); 
     for(let j = asteroids.length-1; j >= 0; j--){
     if (lasers[i].hits(asteroids[j])){
+      if(asteroids[j].r > 10){
+
       let newAsteroids = asteroids[j].breakup();
-      console.log(newAsteroids);
-      //asteroids.push(newAsteroids);
+      asteroids = asteroids.concat(newAsteroids);
+    }
       asteroids.splice(j,1);
       lasers.splice(i,1);
       break;
